136ef93ce34d248c8f6f735910e12e292f466f42
YaEHOTUK <YaEHOTUK@github.com>
2026-07-07 13:57:46 +0400

    add backpack and garbage-dungeon system

--- text_rpg_roguelike.py
+++ text_rpg_roguelike.py
@@ -16,13 +16,54 @@ if choice == "1":
     name = 'ЕНОТ'
     hp = 100
     atk = 15
+    hero_energy = 60
 elif choice == "2":
     name = 'Голодный ЕНОТ'
     hp = 60
     atk = 25
+    hero_energy = 80
 else:
    print("\n⚠️ Ошибка детекции IQ! Обнаружен скрытый класс...") 
    name = 'Дурачонок'
    hp = 20
    atk = 6
+   hero_energy = 140
 print(f"{name}, здоровье =  {hp}, атака = {atk} ")
+
+import random
+
+max_energy = hero_energy
+current_energy = hero_energy
+print(f"⚡ Текущая энергия Енота: {current_energy}/{max_energy}")
+
+
+backpack = []
+while current_energy >= 0:
+    print("🦝🦝🦝 Енот отправляется в данж 'Помойка'...🦝🦝🦝")
+    print(f"⚡ Вход оплачен! Остаток энергии: {current_energy}")
+    item_type = random.randint(1, 3)
+    current_energy = current_energy - 20
+    if item_type == 1:
+        loot_hp = random.randint(-10, -1)
+        loot_atk = random.randint(10, 20)
+        item_name = (f"Круассан с шоколадом (хп: {loot_hp}, атака: {loot_atk})")
+    elif item_type == 2:
+        loot_hp = random.randint(15, 30)
+        loot_atk = random.randint(-10, -5)
+        item_name = (f"Упаковка бекона (хп: {loot_hp}, атака: {loot_atk})")
+    else:
+        loot_hp = random.randint(-25, -15)
+        loot_atk = random.randint(-20, -10)
+        item_name = (f"Разложившийся кусок пиццы (хп: {loot_hp}, атака: {loot_atk})")
+
+    backpack.append(item_name)
+    print(item_name)
+    print(f"\n 🟢 Содержимое сумки: {backpack}")
+    action = input("\n🤔 Надеть этот артефакт? (1 — Да, 2 — Выбросить):")
+    if action == "1":
+        hp = hp + loot_hp
+        atk = atk + loot_atk
+        backpack.append(item_name)
+        print(f"✅ Надето! Текущие статы {name}: hp={hp}, atk={atk}")
+    else:
+        print("🗑️ ЗАЧЕМ ВЫБРАСЫВАТЬ ЕДУ????? ТЫ ЧТО, 'Дурачонок'?")
